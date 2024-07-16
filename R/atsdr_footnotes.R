aef8d3c2ec68fc9412a79705f3a3f381ab0ca2a7
Pyon <qae1@cdc.gov>
2024-07-16 08:40:20 -0400

    loopable for all integer values

--- R/atsdr_footnotes.R
+++ R/atsdr_footnotes.R
@@ -1,19 +1,18 @@
-
+#Footnotes based on ATSDR Guidance
 
 atsdr_footnotes <- function(i = 0) {
 
-  key_vctr <- c("*", "†", "‡", "§", "¶", "**", "††", "‡‡", "§§", "¶¶")
-
-  if(sum(i == 0:10) == 0) {stop("i must be within [0:10]")}
+  key_vctr <- c("*", "†", "‡", "§", "¶")
 
   if(i == 0) {
-
     print(key_vctr)
-
   } else {
-
-    print(key_vctr[as.numeric(i)])
-
+    index <- as.numeric(i)-1
+    integer <- index%/%5
+    remainder <- index%%5
+    symbol <- key_vctr[remainder+1]
+    vector <- rep(symbol, each = integer+1)
+    print(paste(vector, collapse = ""))
   }
 
 }
