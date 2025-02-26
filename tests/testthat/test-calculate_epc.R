3444bf21efea568e70f22d73b1612039f0801953
Pyon <qae1@cdc.gov>
2025-02-26 15:06:17 -0500

    make test

--- tests/testthat/test-calculate_epc.R
+++ tests/testthat/test-calculate_epc.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("calculate_epc() calculates epc", {
+  results <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+  nondetects <- results<8
+  expect_equal(calculate_epc(obs = results, cen = nondetects)$epc, 10.090688)
 })
