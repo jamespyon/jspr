51bb6c8dc789572e1955c0b3e4e4c8af8835b213
qae1 <qae1@cdc.gov>
2026-01-22 11:04:50 -0500

    update from gitlab

--- R/flextable_atsdr_footnote.R
+++ R/flextable_atsdr_footnote.R
@@ -23,9 +23,9 @@
 #'   flextable::set_caption(flextable::as_paragraph("Iris Title"))
 #'
 #' add_footnotes <- data.frame(
-#'   comment = c("column 3", "location (2,3)", "location (1,1)", "column 2", "title"),
-#'   i = c(NA, 2, 1, NA, NA),
-#'   j = c(3, 3, 1, 2, NA)
+#'   comment = c("col", "cell 1", "cell 1", "col", "title", "cell 2", "col 2", "cell 3", "cell 4"),
+#'   i = c(NA, 2, 1, NA, NA, 3, NA, 4, 6),
+#'   j = c(3, 3, 1, 2, NA, 3, 4, 4, 4)
 #' )
 #'
 #' #example_dataset |> flextable_atsdr_footnote(add_footnotes)
@@ -33,34 +33,37 @@
 
 flextable_atsdr_footnote <- function(flextable, data) {
 
-  #find title footnotes
+  # find title footnotes
   title_fn <- data[is.na(data$i) & is.na(data$j),]
 
-  #find column footnotes and order
+  # find column footnotes and order
   column_fn <- data[is.na(data$i) & !is.na(data$j),]
   column_fn <- column_fn[with(column_fn, order(j)),]
 
-  #find cell footnotes and order
+  # find cell footnotes and order
   cell_fn <- data[!is.na(data$i),]
   cell_fn <- cell_fn[with(cell_fn, order(j,i)),]
 
-  #initialize
+  # initialize
   current_title <- flextable$caption$value
   c_index <- max(current_title$.chunk_index)
   fn_index <- 0
   temp <- flextable
+  save_data <- dplyr::distinct(data, comment)
 
-  #start with title footnotes
+  # start with title footnotes
   for(t in 1:nrow(title_fn)) {
 
-    #current title info
+    # current title info
     title_info <- title_fn[t,]
     format_info <- current_title[1,]
 
-    #increase index
+    # increase index
     fn_index <- fn_index+1
     c_index <- c_index+1
 
+    # initial save comment and index (there shouldn't be the same comment on the title, i assume?)
+    save_data$fn_index[save_data$comment == title_info$comment] <- fn_index
 
     if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
 
@@ -92,65 +95,139 @@ flextable_atsdr_footnote <- function(flextable, data) {
 
   }
 
-  #start with only column footnotes
+  # start with only column footnotes
   for(c in 1:nrow(column_fn)) {
 
-    #current column info
+    # current column info
     column_info <- column_fn[c,]
 
-    #increase index
+    # increase index
     fn_index <- fn_index+1
 
-    if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+    # check and save comment and index
+    save_data_index <- match(column_info$comment, save_data$comment)
 
-      temp <- flextable::footnote(
-        temp,
-        j = column_info$j,
-        part = "header",
-        ref_symbols = atsdr_footnote_symbol(fn_index),
-        value = flextable::as_paragraph(column_info$comment))
+    if(is.na(save_data[save_data_index,]$fn_index)) { #same
+
+      save_data$fn_index[save_data$comment == column_info$comment] <- fn_index
+      fn_index <- fn_index
+
+      if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+
+        temp <- flextable::append_chunks(temp,
+                                         j = column_info$j,
+                                         part = "header",
+                                         flextable::as_chunk(atsdr_footnote_symbol(fn_index)))
+
+        temp <- flextable::add_footer_lines(temp,
+                                            flextable::as_paragraph(atsdr_footnote_symbol(fn_index),
+                                                                    column_info$comment))
+
+      } else {
+
+        temp <- flextable::append_chunks(temp,
+                                         j = column_info$j,
+                                         part = "header",
+                                         flextable::as_sup(atsdr_footnote_symbol(fn_index)))
+
+        temp <- flextable::add_footer_lines(temp,
+                                            flextable::as_paragraph(flextable::as_sup(atsdr_footnote_symbol(fn_index)),
+                                                                    column_info$comment))
+
+      }
 
     } else {
 
-      temp <- flextable::footnote(
-        temp,
-        j = column_info$j,
-        part = "header",
-        ref_symbols = atsdr_footnote_symbol(fn_index),
-        value = flextable::as_paragraph(column_info$comment))
+      fn_index <- save_data$fn_index[save_data$comment == column_info$comment]
+
+      if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+
+        temp <- flextable::append_chunks(temp,
+                                         j = column_info$j,
+                                         part = "header",
+                                         atsdr_footnote_symbol(fn_index))
+
+      } else {
+
+        temp <- flextable::append_chunks(temp,
+                                         j = column_info$j,
+                                         part = "header",
+                                         flextable::as_sup(atsdr_footnote_symbol(fn_index)))
+
+      }
 
     }
+
+    # reset footnote index
+    fn_index <- max(save_data$fn_index, na.rm = TRUE)
 
   }
 
-  #start with only cell footnotes
+  # start with only cell footnotes
   for(d in 1:nrow(cell_fn)) {
 
-    #current cell info
+    # current cell info
     cell_info <- cell_fn[d,]
 
-    #increase index
+    # increase index
     fn_index <- fn_index+1
 
-    if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+    # check and save comment and index
+    save_data_index <- match(cell_info$comment, save_data$comment)
 
-      temp <- flextable::footnote(
-        temp,
-        i = cell_info$i, j = cell_info$j,
-        part = "body",
-        ref_symbols = atsdr_footnote_symbol(fn_index),
-        value = flextable::as_paragraph(cell_info$comment))
+    if(is.na(save_data[save_data_index,]$fn_index)) {
+
+      save_data$fn_index[save_data$comment == cell_info$comment] <- fn_index
+      fn_index <- fn_index
+
+      if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+
+        temp <- flextable::append_chunks(temp,
+                                         i = cell_info$i, j = cell_info$j,
+                                         part = "body",
+                                         flextable::as_chunk(atsdr_footnote_symbol(fn_index)))
+
+        temp <- flextable::add_footer_lines(temp,
+                                            flextable::as_paragraph(atsdr_footnote_symbol(fn_index),
+                                                                    cell_info$comment))
+
+      } else {
+
+        temp <- flextable::append_chunks(temp,
+                                         i = cell_info$i, j = cell_info$j,
+                                         part = "body",
+                                         flextable::as_sup(atsdr_footnote_symbol(fn_index)))
+
+        temp <- flextable::add_footer_lines(temp,
+                                            flextable::as_paragraph(flextable::as_sup(atsdr_footnote_symbol(fn_index)),
+                                                                    cell_info$comment))
+
+      }
 
     } else {
 
-      temp <- flextable::footnote(
-        temp,
-        i = cell_info$i, j = cell_info$j,
-        part = "body",
-        ref_symbols = atsdr_footnote_symbol(fn_index),
-        value = flextable::as_paragraph(cell_info$comment))
+      fn_index <- save_data$fn_index[save_data$comment == cell_info$comment]
+
+      if(grepl("\\*", atsdr_footnote_symbol(fn_index))) { #do not superscript star symbol
+
+        temp <- flextable::append_chunks(temp,
+                                         i = cell_info$i, j = cell_info$j,
+                                         part = "body",
+                                         atsdr_footnote_symbol(fn_index))
+
+      } else {
+
+        temp <- flextable::append_chunks(temp,
+                                         i = cell_info$i, j = cell_info$j,
+                                         part = "body",
+                                         flextable::as_sup(atsdr_footnote_symbol(fn_index)))
+
+      }
 
     }
+
+    # reset footnote index
+    fn_index <- max(save_data$fn_index, na.rm = TRUE)
 
   }
 
@@ -159,4 +236,3 @@ flextable_atsdr_footnote <- function(flextable, data) {
   return(output)
 
 }
-
